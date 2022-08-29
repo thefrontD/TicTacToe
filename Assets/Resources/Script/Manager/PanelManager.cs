4410e0f3a7fa157c43007670672a5a028cce168c
thefrontD_desktop <amigojmk@gmail.com>
2022-08-30 06:00:15 +0900

    대상 선택 안내문 작업
    이동시, 색칠시, 공격시 대상 선택하도록 하는 안내문 생성

--- Assets/Resources/Script/Manager/PanelManager.cs
+++ Assets/Resources/Script/Manager/PanelManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PanelManager : Singleton<PanelManager>
 {
@@ -13,6 +14,25 @@ public class PanelManager : Singleton<PanelManager>
     [SerializeField] private Button nextButton;
     [SerializeField] private Button mainMenuButton;
     [SerializeField] private Button exitGameButton;
+    public GameObject DirectionNotice;
+    private string MoveNotice = "이동할 위치를 선택하세요";
+    private string AttackNotice = "공격할 대상을 선택하세요";
+    private string ColorNotice = "색칠할 위치를 선택하세요";
+
+    public void SetDirectionNotice(States state){
+        switch(state){
+            case States.Attack:
+                DirectionNotice.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = AttackNotice;
+                break;
+            case States.Move:
+                DirectionNotice.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = MoveNotice;
+                break;
+            case States.Color:
+                DirectionNotice.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = ColorNotice;
+                break;
+        }
+        return;
+    }
 
     void Start()
     {
